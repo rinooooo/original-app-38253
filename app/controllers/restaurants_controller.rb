10d967be029a4c4d79ae0aafa362a9197d596756
rinooooo <rino.0566@gmail.com>
2022-08-03 22:41:54 +0900

    店登録機能の実装(画像・tag以外)
    アクティブハッシュを利用

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,5 +3,23 @@ class RestaurantsController < ApplicationController
 
   def index
   end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def restaurant_params
+    params.require(:restaurant).permit(:shop_name, :address, :category_id, :phone_number, :url).merge(user_id: current_user.id)
+  end
 end
 
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,8 @@
 class Restaurant < ApplicationRecord
   belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
 end
