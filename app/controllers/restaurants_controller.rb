110d353eacce28d5873862eb94176fa06c6ac614
rinooooo <rino.0566@gmail.com>
2022-08-03 17:55:02 +0900

    Userモデル単体テスト
    validationに対して

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,5 @@
 class RestaurantsController < ApplicationController
+  before_action :authenticate_user!, expect: [:index]
 
   def index
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :group_name, presence: true
+  
   has_many :restaurants
 end
