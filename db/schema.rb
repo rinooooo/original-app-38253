9438c5e9a091a453c620531b68089e92d7e8cd97
rinooooo <rino.0566@gmail.com>
2022-08-03 13:50:53 +0900

    User/Restaurantモデルのアソシエーション
    restaurantsテーブルにuser_idカラム追加
    各モデルにアソシエーションの記述

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_03_034258) do
+ActiveRecord::Schema.define(version: 2022_08_03_020050) do
 
   create_table "restaurants", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "shop_name", null: false
@@ -18,8 +18,10 @@ ActiveRecord::Schema.define(version: 2022_08_03_034258) do
     t.integer "category_id", null: false
     t.string "phone_number"
     t.string "url"
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_restaurants_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -35,4 +37,5 @@ ActiveRecord::Schema.define(version: 2022_08_03_034258) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "restaurants", "users"
 end
