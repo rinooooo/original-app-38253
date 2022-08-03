f112e42ea41b5c6c2d0ff0210cebec85cb816644
rinooooo <rino.0566@gmail.com>
2022-08-03 22:40:42 +0900

    店登録ページの編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "restaurants#index"
-  resources :restaurants, only: [:index]
+  resources :restaurants, only: [:index, :new, :create]
 end
