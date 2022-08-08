acb3e560e9d21397f47ad30f3ee86c42a32f30dd
rinooooo <rino.0566@gmail.com>
2022-08-08 14:30:12 +0900

    店詳細ページGoogleマップ表示

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_08_022232) do
+ActiveRecord::Schema.define(version: 2022_08_08_052904) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -73,7 +73,6 @@ ActiveRecord::Schema.define(version: 2022_08_08_022232) do
 
   create_table "restaurants", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "shop_name", null: false
-    t.string "address"
     t.integer "category_id", null: false
     t.string "phone_number"
     t.string "url"
